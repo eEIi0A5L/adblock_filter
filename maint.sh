4bdabe7c0fa011ab772db2f331bc02a9caa9ccfe
mochifilter <nolamiller1203+githubmochifilter@gmail.com>
2018-02-25 08:42:25 +0900

    一旦コミット

--- maint.sh
+++ maint.sh
@@ -3,5 +3,5 @@
 LIST=`ls *.txt`
 for i in $LIST; do
 	echo "=====[$i]====="
-	grep -v "##" $i | grep -v "||" | grep -v -E "^!" | grep -v "domain"
+	grep -v -E ".+##" $i | grep -v "||" | grep -v -E "^!" | grep -v "domain"
 done
