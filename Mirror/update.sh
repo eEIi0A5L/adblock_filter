e01ae5c7dcd3c7af4aae43a80ee16a8c573364b4
eEIi0A5L <nolamiller1203+githubmochifilter@gmail.com>
2018-03-27 02:20:44 +0900

    mod

---
+++ Mirror/update.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+wget http://tofukko.r.ribbon.to/Adblock_Plus_list.txt
+wget http://tofukko.r.ribbon.to/Adblock_Plus_list_sjis.txt
+wget http://www.malwaredomainlist.com/hostslist/hosts.txt -O MalwareDomainList.com.txt
