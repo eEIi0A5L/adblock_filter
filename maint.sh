111d30d585d8763749524af22b0dafe528983ad9
eEIi0A5L <nolamiller1203+githubmochifilter@gmail.com>
2018-12-15 02:48:27 +0900

    grep重すぎ県

--- maint.sh
+++ maint.sh
@@ -1,7 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-LIST=`ls *.txt`
-for i in $LIST; do
-	echo "=====[$i]====="
-	grep -v -E "##" $i | grep -v "||" | grep -v -E "^[ \t]*!" | grep -v "domain" | grep -v "#@#" | grep -v -E "^$"
-done
+function sub()
+{
+	FILE=$1
+	echo "----$FILE"
+	#grep -v -E "##" $i | grep -v "||" | grep -v -E "^[ \t]*!" | grep -v "domain" | grep -v "#@#" | grep -v -E "^$"
+	while IFS=$'\r' read line; do
+		if [[ $line =~ \#\# ]]; then
+			continue
+		fi
+		if [[ $line =~ ^\r\n ]]; then
+			continue
+		fi
+		if [[ $line =~ ^$ ]]; then
+			continue
+		fi
+		if [[ $line =~ ^\[Adblock ]]; then
+			continue
+		fi
+		if [[ $line =~ \|\| ]]; then
+			continue
+		fi
+		#if [[ $line =~ ^\[ \\t\]\*\! ]]; then
+		regexp='!'
+		if [[ $line =~ $regexp ]]; then
+			continue
+		fi
+		if [[ $line =~ domain ]]; then
+			continue
+		fi
+		if [[ $line =~ \#@\# ]]; then
+			continue
+		fi
+		#echo "[$line]"
+		echo "$line"
+	done < $FILE
+}
+
+function main()
+{
+	for i in *.txt; do
+		#echo $i
+		sub $i
+	done
+}
+main
