70f2ffee93c4de5e4cb234d3824e12d00e7fa94a
Your Name <you@example.com>
2019-01-18 13:07:08 +0900

    mod

--- cm.sh
+++ cm.sh
@@ -1,6 +1,33 @@
 #!/bin/bash
 # cm - cosmetic maint
 
+# cosmetic_partにさらに##があるならエラー
+function check1()
+{
+	domain_part=$1
+	cosmetic_part=$2
+	#echo "$domain_part"
+	#echo "$cosmetic_part"
+	if [[  $cosmetic_part =~ \#\# ]]; then
+		echo "$line"
+		echo "ERROR"
+	fi
+}
+
+# 要素のAttributeの値にクオテーションが使われてないならエラー
+function check2()
+{
+	domain_part=$1
+	cosmetic_part=$2
+	#if [[  $cosmetic_part =~ \[[a-z]\$?=([\"\']) ]]; then
+	#	return
+	#fi
+	if [[  $cosmetic_part =~ \[[a-z]+\$?=([^\"\']) ]]; then
+		echo "$line"
+		echo "ERROR"
+	fi
+}
+
 function sub()
 {
 	FILE=$1
@@ -18,11 +45,8 @@ function sub()
 		cosmetic_part=${line##*\#\#}
 		#echo "$cosmetic_part"
 
-		if [[ ! $cosmetic_part =~ \#\# ]]; then
-			continue
-		fi
-		echo "$line"
-		echo "ERROR"
+		check1 $domain_part $cosmetic_part 
+		check2 $domain_part $cosmetic_part 
 
 	done < $FILE
 }
