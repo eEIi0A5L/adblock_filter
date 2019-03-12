d3f1d53fbfe77e0e38e98db81db44e84c577ce01
Your Name <you@example.com>
2019-03-13 07:54:01 +0900

    mod

--- Mirror/update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wget http://tofukko.r.ribbon.to/Adblock_Plus_list.txt -O Adblock_Plus_list.txt
-wget http://tofukko.r.ribbon.to/Adblock_Plus_list_sjis.txt -O Adblock_Plus_list_sjis.txt
-wget http://www.malwaredomainlist.com/hostslist/hosts.txt -O MalwareDomainList.com.txt
-wget https://hosts-file.net/ad_servers.txt -O ad_servers.txt
